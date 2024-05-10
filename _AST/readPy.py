db4673c522321e5806badc628dd9018065c59615
uni03957 <62983154+uni03957@users.noreply.github.com>
2024-05-11 01:29:46 +0900

    Update readPy.py
    Add Comment about Ver 1.0 code

--- _AST/readPy.py
+++ _AST/readPy.py
@@ -1,6 +1,19 @@
 code = []
 
 with open("test.txt", "r") as f: #코드 원형으로 갖고 옴.
-    code = list(map(lambda line: line.rstrip(),f.readlines()))
+    code = list(map(lambda line: line.rstrip(),f.readlines())) #줄바꿈 없애기 (By using Lambda)
 
 print(code)
+
+
+
+
+"""
+[This Code's Ver 1.0]
+code = []
+
+with open("test.txt", "r") as f: #코드 원형으로 갖고 옴.
+    for i in f.readlines():
+        i = i.strip("\n") #줄바꿈만 없앰.
+        code.append(i)
+"""
